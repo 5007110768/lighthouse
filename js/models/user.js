6752683bfd4bc342aa565795c14b73e146b1c561
Jamie Jansen <jamie.jansen@live.com>
2017-06-19 22:23:59 +0200

    updated user model

--- js/models/user.js
+++ js/models/user.js
@@ -1,4 +1,4 @@
-let User = function(id, email, firstName, lastName, gender, dateOfBirth, description, interests, partnerPreferences, photos, matches) {
+let User = function(id, email, firstName, lastName, gender, dateOfBirth, description, interests, partnerPreferences, photos, matches, permission) {
     this.id  = id; // Number
     this.email = email; // String
     this.firstName = firstName; // String
@@ -10,4 +10,5 @@ let User = function(id, email, firstName, lastName, gender, dateOfBirth, descrip
     this.partnerPreferences = partnerPreferences; // Interests object
     this.photos = photos; // Array of img urls or img data files
     this.matches = matches; // Array of userId's
+    this.permission = permission; // Number (int)
 };
